882cbe7179c68e672e04234f333d8c8e4a2cfab8
H1rono <hronok66@gmail.com>
2023-06-20 06:28:22 +0900

    :sparkles: Add module payloads::serde::time

--- src/payloads.rs
+++ src/payloads.rs
@@ -2,6 +2,7 @@
 
 mod channel;
 mod message;
+mod serde;
 mod stamp;
 mod system;
 mod tag;
---
+++ src/payloads/serde.rs
@@ -0,0 +1,38 @@
+#[cfg(feature = "time")]
+pub mod time {
+    pub use time::serde::rfc3339::{deserialize, serialize};
+}
+
+#[cfg(not(feature = "time"))]
+pub mod time {
+    use serde::{
+        de::{Error, Visitor},
+        Deserializer, Serialize, Serializer,
+    };
+
+    pub fn serialize<S: Serializer>(data: &String, serializer: S) -> Result<S::Ok, S::Error> {
+        data.serialize(serializer)
+    }
+
+    pub fn deserialize<'a, D: Deserializer<'a>>(deserializer: D) -> Result<String, D::Error> {
+        deserializer.deserialize_string(StringVisitor)
+    }
+
+    struct StringVisitor;
+
+    impl<'de> Visitor<'de> for StringVisitor {
+        type Value = String;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            write!(formatter, "a string")
+        }
+
+        fn visit_string<E: Error>(self, v: String) -> Result<Self::Value, E> {
+            Ok(v)
+        }
+
+        fn visit_str<E: Error>(self, v: &str) -> Result<Self::Value, E> {
+            Ok(String::from(v))
+        }
+    }
+}
