6f5b5e55fc147a789ea83d3d0f5eec14a3240b6c
H1rono <hronok66@gmail.com>
2023-11-27 21:38:58 +0900

    :tada: Init Nix flake and direnv

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Library to parse POST requests of traQ BOT event";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix/monthly";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, fenix, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-S4dA7ne2IpFHG+EnjXfogmqwGyDFSRWFnJ8cy4KZr1k=";
+        };
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.stdenv.mkDerivation {
+          name = "rust-env";
+          nativeBuildInputs = with pkgs; [
+            toolchain
+            grcov # to collect coverage
+            libiconv # required to build example `basic-with-axum`
+          ];
+        };
+      }
+    );
+}
