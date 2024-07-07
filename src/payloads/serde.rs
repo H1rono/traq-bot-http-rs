1cf6508145d6c98680814d22a8c6f2dc54111b31
H1rono <hronok66@gmail.com>
2024-07-07 13:05:32 +0900

    :recycle: Less `cfg`s

--- src/payloads/serde.rs
+++ src/payloads/serde.rs
@@ -1,6 +1,16 @@
 //! `#[serde(with = "...")]`で使用するためのモジュール群
 
-#[cfg(feature = "chrono")]
+#[cfg(not(feature = "chrono"))]
+#[cfg(feature = "time")]
+/// タイムスタンプ型[`crate::payloads::types::TimeStamp`]用のモジュール
+pub mod timestamp {
+    pub use time::serde::rfc3339::{deserialize, serialize};
+}
+
+#[cfg(any(
+    all(not(feature = "chrono"), not(feature = "time")),
+    feature = "chrono"
+))]
 /// タイムスタンプ型[`crate::payloads::types::TimeStamp`]用のモジュール
 pub mod timestamp {
     use serde::{Deserialize, Deserializer, Serialize, Serializer};
@@ -13,27 +23,5 @@ pub mod timestamp {
 
     pub fn deserialize<'a, D: Deserializer<'a>>(deserializer: D) -> Result<TimeStamp, D::Error> {
         TimeStamp::deserialize(deserializer)
-    }
-}
-
-#[cfg(not(feature = "chrono"))]
-#[cfg(feature = "time")]
-/// タイムスタンプ型[`crate::payloads::types::TimeStamp`]用のモジュール
-pub mod timestamp {
-    pub use time::serde::rfc3339::{deserialize, serialize};
-}
-
-#[cfg(not(feature = "chrono"))]
-#[cfg(not(feature = "time"))]
-/// タイムスタンプ型[`crate::payloads::types::TimeStamp`]用のモジュール
-pub mod timestamp {
-    use serde::{Deserialize, Deserializer, Serialize, Serializer};
-
-    pub fn serialize<S: Serializer>(data: &String, serializer: S) -> Result<S::Ok, S::Error> {
-        data.serialize(serializer)
-    }
-
-    pub fn deserialize<'a, D: Deserializer<'a>>(deserializer: D) -> Result<String, D::Error> {
-        String::deserialize(deserializer)
     }
 }
