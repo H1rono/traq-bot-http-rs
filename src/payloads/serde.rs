fa9b6a5fe8dc4915566db4c60d0b639b8faa2d12
H1rono <hronok66@gmail.com>
2024-07-07 12:26:26 +0900

    :fire: Remove `StringVisitor`

--- src/payloads/serde.rs
+++ src/payloads/serde.rs
@@ -27,32 +27,13 @@ pub mod timestamp {
 #[cfg(not(feature = "time"))]
 /// タイムスタンプ型[`crate::payloads::types::TimeStamp`]用のモジュール
 pub mod timestamp {
-    use serde::de::{Error, Visitor};
-    use serde::{Deserializer, Serialize, Serializer};
-
-    struct StringVisitor;
-
-    impl<'de> Visitor<'de> for StringVisitor {
-        type Value = String;
-
-        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-            write!(formatter, "a string")
-        }
-
-        fn visit_string<E: Error>(self, v: String) -> Result<Self::Value, E> {
-            Ok(v)
-        }
-
-        fn visit_str<E: Error>(self, v: &str) -> Result<Self::Value, E> {
-            Ok(String::from(v))
-        }
-    }
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
     pub fn serialize<S: Serializer>(data: &String, serializer: S) -> Result<S::Ok, S::Error> {
         data.serialize(serializer)
     }
 
     pub fn deserialize<'a, D: Deserializer<'a>>(deserializer: D) -> Result<String, D::Error> {
-        deserializer.deserialize_string(StringVisitor)
+        String::deserialize(deserializer)
     }
 }
